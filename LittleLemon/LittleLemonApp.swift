ee8e573ac5b6a6cb6275b4a1a74f6d3207aac967
Josiah Agosto <agosto_josiah@yahoo.com>
2023-04-22 14:58:50 -0400

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Josiah Agosto on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
